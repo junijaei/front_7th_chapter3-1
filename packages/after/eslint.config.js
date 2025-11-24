f4d1319a73cfd0b4781aa1f2dbed23808c3246e4
junijaei <hjhj7895598@gmail.com>
2025-11-24 23:34:39 +0900

    style: lint, prettier 규칙 추가

--- packages/after/eslint.config.js
+++ packages/after/eslint.config.js
@@ -6,6 +6,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -17,7 +20,19 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: {
+      'no-relative-import-paths': noRelativeImportPaths,
+      prettier,
+    },
+    rules: {
+      'no-relative-import-paths/no-relative-import-paths': [
+        'error',
+        { allowSameFolder: false, rootDir: 'src', prefix: '@' }
+      ],
+      'prettier/prettier': 'error',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
